20888073cc35ca1827e5b9a57a992279149f3e87
John <jiao.xuejiao@gmail.com>
2016-09-27 13:40:39 +0800

    delete todo-add,edit&update

--- routes/todo.py
+++ routes/todo.py
@@ -9,34 +9,3 @@ main = Blueprint('todo', __name__)
 def index():
     ts = Todo.query.all()
     return  render_template('todo_index.html', todo_list=ts)
-
-
-@main.route('/edit/<id>')
-def edit(id):
-    t = Todo.query.get(id)
-    # t = Todo.query.filter_by(id=id).first()
-    return render_template('todo_edit.html', todo=t)
-
-
-@main.route('/add', methods=['POST'])
-def add():
-    form = request.form
-    t = Todo(form)
-    print('form', form)
-    t.save()
-    return redirect(url_for('.index'))
-
-
-@main.route('/update/<int:id>', methods=['POST'])
-def update(id):
-    form = request.form
-    t = Todo.query.get(id)
-    t.update(form)
-    return redirect(url_for('.index'))
-
-
-@main.route('/delete/<int:id>')
-def delete(id):
-    t = Todo.query.get(id)
-    t.delete()
-    return redirect(url_for('.index'))
